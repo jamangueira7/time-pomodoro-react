eec34b8afef1153f524f2407582cdc3ef0c39499
João Mangueira <jamjs7@gmail.com>
2023-05-23 16:30:21 -0300

    add layout do historico

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -1,33 +1,62 @@
 import styled from "styled-components";
 
-export const HeaderContainer = styled.div`
+export const HistoryContainer = styled.main`
+  flex: 1;
+  padding: 3.5rem;
   display: flex;
-  align-items: center;
-  justify-content: space-between;
+  flex-direction: column;
   
-  nav {
-    display: flex;
-    gap: 0.5rem;
+  h1 {
+    font-size: 1.5rem;
+    color: ${(props) => props.theme['gray-100']};
+  }
+`;
+
+export const HistoryList = styled.div`
+  flex: 1;
+  overflow: auto;
+  margin-top: 2rem;
+  
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    min-width: 600px;
     
-    a {
-      width: 3rem;
-      height: 3rem;
-      
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      
+    th {
+      background: ${(props) => props.theme['gray-600']};
+      padding: 1rem;
+      text-align: left;
       color: ${(props) => props.theme['gray-100']};
-      border-top: 3px solid transparent;
-      border-bottom: 3px solid transparent;
+      font-size: 0.875rem;
+      line-height: 1.6;
       
-      &:hover {
-        border-bottom: 3px solid ${(props) => props.theme['green-500']};
+      &:first-child {
+        border-top-left-radius: 8px;
+        padding-left: 1.5rem;
       }
-      
-      &.active {
-        color: ${(props) => props.theme['green-500']};
+
+      &:last-child {
+        border-top-right-radius: 8px;
+        padding-right: 1.5rem;
+      }
+    }
+    
+    td {
+      background-color: ${(props) => props.theme['gray-700']};
+      border-top: 4px solid ${(props) => props.theme['gray-800']};
+      padding: 1rem;
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      &:first-child {
+        width: 50%;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        padding-right: 1.5rem;
       }
     }
   }
 `;
+
