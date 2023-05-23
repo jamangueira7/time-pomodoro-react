f15eab26da43a901938d639c33adce67c44da623
João Mangueira <jamjs7@gmail.com>
2023-05-23 10:32:08 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { ThemeProvider } from "styled-components";
+import { Button } from "./components/Button.tsx";
+import {defaultTheme} from "./styles/themes/default.ts";
+import {GlobalStyle} from "./styles/global.ts";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary"/>
+      <Button variant="secondary"/>
+      <Button variant="success"/>
+      <Button variant="danger"/>
+      <Button />
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
+
